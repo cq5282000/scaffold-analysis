a0a8384fecb25a6671353e287e23b24b73f26503
cq5282000 <18801911877@163.com>
2018-12-05 19:03:17 +0800

    add

--- bin/hello.js
+++ bin/hello.js
@@ -1,29 +1,21 @@
 #!/usr/bin/env node
 
-// console.log(process.argv[0]);
-// console.log(process.argv[1]);
-// console.log(process.argv[2]);
-// console.log(process.argv[3]);
-
-const regex = /^NODE_ENV=(.*)$/g;
-
 /**
  * 获取环境变量env
  */
-let env = 'production';
-
-process.argv[3] &&
-process.argv[3].match(regex).length !== 0 &&
-(env = process.argv[3].match(regex)[0].split('=')[1]);
-
-console.log(process.env.NODE_ENV);
-
+const path = require('path');
+let env = process.env.NODE_ENV || 'production';
+const { execSync } = require('child_process');
+const ROOT_PATH = process.cwd();
+const SCAFFOLD_ANALYSIS = path.dirname(path.resolve(require.main.filename, '..'));
 switch (process.argv[2]) {
     case 'start':
         console.log('hello statr');
+
         break;
     case 'build':
         console.log('hello build');
+        execSync(`gulp --gulpfile ${SCAFFOLD_ANALYSIS}/gulpfle.js`);
         break;
     default:
         break;
