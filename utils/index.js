63dbea800d3b13aa7f3cb9365e014cb12b18ad8e
cq5282000 <18801911877@163.com>
2018-12-06 15:19:25 +0800

    add

--- utils/index.js
+++ utils/index.js
@@ -1,7 +1,46 @@
-const glob = require('glob');
-const path = require('path');
+const fs = require('fs');
+const pageConfig = {};
 
-glob.sync('../!(node_modules)/').map((file) => {
-    console.log(file);
-    console.log(file.split('/').slice(-2, -1));
-});
+/**
+ * 判断文件是否有缺失
+ * @param {modulePath} viewPath
+ */
+function fileIsLostOrNot(modulePath) {
+    return [`${modulePath}/action.js`, `${modulePath}/index.js`, `${modulePath}/reducer.js`].some((item) => {
+        return !fs.existsSync(item);
+    });
+}
+/**
+ * 生成页面配置
+ * @param {*} viewPath
+ */
+function generateConfig(viewPath) {
+    const glob = require('glob');
+    const path = require('path');
+
+    glob.sync(`${viewPath}/*/*`).map((file) => {
+        // console.log(file);
+        if (fileIsLostOrNot(file)) {
+            // throw new Error(`something is wrong with ${file}`);
+            return;
+        }
+        const fileArr = file.split('/').slice(-3, -1);
+        if (pageConfig[fileArr[0]]) {
+            pageConfig[fileArr[0]].push(fileArr[1]);
+        } else {
+            pageConfig[fileArr[0]] = [];
+            pageConfig[fileArr[0]].push(fileArr[1]);
+        }
+    });
+    return pageConfig;
+}
+
+/**
+ * 写文件
+ */
+function writeFile(srcPath, distPath, data) {
+    //
+}
+module.exports = {
+    generateConfig
+};
